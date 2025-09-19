d9804322e8e1fec1ef6fadf8351f16e70f9b60e3
Bruno Abreu <vazs.bruno@gmail.com>
2025-09-19 17:14:12 -0300

    feat: estrutura do projeto

---
+++ src/test/java/br/com/brunovaz/gestao_vagas/modules/candidate/controllers/CandidateControllers.java
@@ -0,0 +1,25 @@
+package br.com.brunovaz.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import br.com.brunovaz.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+
+
+
+
+@RestController
+@RequestMapping ("/candidate")
+public class CandidateControllers {
+
+    @PostMapping("/")
+    public void create( @Valid @RequestBody CandidateEntity candidateEntity){
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
+
+    }
+    
+}
