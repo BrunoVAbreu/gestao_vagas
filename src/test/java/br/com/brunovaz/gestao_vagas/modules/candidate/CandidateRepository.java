50c7c1baa76506da56b254c2f9e5ec417a3985c9
Bruno Abreu <vazs.bruno@gmail.com>
2025-09-20 09:01:50 -0300

    feat: use case para validar usuário

--- src/test/java/br/com/brunovaz/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/test/java/br/com/brunovaz/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -1,9 +1,11 @@
 package br.com.brunovaz.gestao_vagas.modules.candidate;
 
+
 import java.util.UUID;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
-    
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
 }
