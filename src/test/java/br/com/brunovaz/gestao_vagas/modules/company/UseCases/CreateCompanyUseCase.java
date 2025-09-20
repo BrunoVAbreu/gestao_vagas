e6e5bee868dc65f4818317a2d4784a0e41c4293a
Bruno Abreu <vazs.bruno@gmail.com>
2025-09-20 18:54:50 -0300

    feat: company e job controllers

--- src/test/java/br/com/brunovaz/gestao_vagas/modules/company/UseCases/CreateCompanyUseCase.java
+++ src/test/java/br/com/brunovaz/gestao_vagas/modules/company/UseCases/CreateCompanyUseCase.java
@@ -1,4 +1,4 @@
-package br.com.brunovaz.gestao_vagas.modules.company.UseCases;
+package br.com.brunovaz.gestao_vagas.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +14,7 @@ public class CreateCompanyUseCase {
     private CompanyRepository companyRepository;
 
     public CompanyEntity execute(CompanyEntity companyEntity){
-        
+
         this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
             .ifPresent((user) -> {
                 throw new UserFoundException();
