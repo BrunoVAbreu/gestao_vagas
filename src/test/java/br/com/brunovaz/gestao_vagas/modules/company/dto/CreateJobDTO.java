e2e422870be164c5629f02902d5abc965abc36e6
Bruno Abreu <vazs.bruno@gmail.com>
2025-09-23 17:07:53 -0300

    refactor: JobController

---
+++ src/test/java/br/com/brunovaz/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,12 @@
+package br.com.brunovaz.gestao_vagas.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+    private String descript;
+    private String benefits;
+    private String level;
+
+    
+}
