a355ec3235003025b0f50da5d98734440909b01a
Bruno Abreu <vazs.bruno@gmail.com>
2025-09-23 18:09:23 -0300

    feat: Criando token de candidato

---
+++ src/test/java/br/com/brunovaz/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,14 @@
+package br.com.brunovaz.gestao_vagas.modules.candidate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCandidateResponseDTO {
+    private String acceess_token;
+}
