96a99cc1c3582a56881f49faf0235511f270d0c3
Bruno Abreu <vazs.bruno@gmail.com>
2025-09-25 20:37:05 -0300

    feat: Criando SwaggerConfig

--- src/test/java/br/com/brunovaz/gestao_vagas/modules/company/controllers/JobController.java
+++ src/test/java/br/com/brunovaz/gestao_vagas/modules/company/controllers/JobController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,6 @@ import br.com.brunovaz.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.brunovaz.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.brunovaz.gestao_vagas.modules.company.useCases.CreateJobUseCase;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -21,8 +21,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-
 
 @RestController
 @RequestMapping("/company/job")
@@ -40,6 +38,7 @@ public class JobController {
             @Content(schema = @Schema(implementation = JobEntity.class))
         })
     })
+    @SecurityRequirement(name = "jwt_auth")
     public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request){
         var companyId = request.getAttribute("company_id");
 
