4c30cfc5cb10042859514cc97e3d924341c7e7a4
Bruno Abreu <vazs.bruno@gmail.com>
2025-09-30 20:05:31 -0300

    test: validando se o job nao existe

--- src/test/java/br/com/brunovaz/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/br/com/brunovaz/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -1,14 +1,22 @@
 package br.com.brunovaz.gestao_vagas.modules.candidate.useCases;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.assertj.core.api.Assertions.assertThat;
 
+import br.com.brunovaz.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.brunovaz.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.brunovaz.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.brunovaz.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.brunovaz.gestao_vagas.modules.company.repositories.JobRepository;
 
@@ -33,8 +41,22 @@ public class ApplyJobCandidateUseCaseTest {
 
         }catch(Exception e){
             assertThat(e).isInstanceOf(UserNotFoundException.class);
-            
         }
-
     }
+    @Test
+    public void shoud_not_be_able_to_apply_job_with_job_not_found(){
+        var idCandidate = UUID.randomUUID();
+
+        var candidate = new CandidateEntity();
+        candidate.setId(idCandidate);
+
+        when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+
+        try{
+            applyJobCandidateUseCase.execute(idCandidate, null);
+        }catch(Exception e){
+            assertThat(e).isInstanceOf(JobNotFoundException.class);
+        }
+    }
+
 }
