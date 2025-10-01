51dd8e03db567eaffd107949fefc07e478f4602b
Bruno Abreu <vazs.bruno@gmail.com>
2025-10-01 19:56:44 -0300

    feat: caso de teste de sucesso

---
+++ src/test/java/br/com/brunovaz/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -0,0 +1,56 @@
+package br.com.brunovaz.gestao_vagas.modules.company.controllers;
+
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import br.com.brunovaz.gestao_vagas.modules.company.dto.CreateJobDTO;
+import br.com.brunovaz.gestao_vagas.utils.TestUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= WebEnvironment.RANDOM_PORT)
+public class CreateJobControllerTest {
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void before(){
+        mvc = MockMvcBuilders
+        .webAppContextSetup(context)
+        .apply(SecurityMockMvcConfigurers.springSecurity())
+        .build();
+    }
+    
+    @Test
+    public void should_be_able_to_create_a_new_job() throws Exception{
+
+        var createJobDTO = CreateJobDTO.builder()
+            .benefits("BENEFITS TEST")
+            .descript("DESCRIPTION_TEST")
+            .level("LEVEL_TEST").build();
+
+        var result = mvc.perform( MockMvcRequestBuilders.post("/company/job/")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtils.objectToJSON(createJobDTO))
+            .header("Authorization", TestUtils.generateToken(UUID.fromString("c8686cf3-d9f2-425d-af82-b39a66968f12"),"JAVAGAS_@123#")))
+            .andExpect(MockMvcResultMatchers.status().isOk());
+
+        System.out.println(result);
+    }    
+}
