cff44b8efaf9ea530ccdc13c9aa6601b5102aab5
Bruno Abreu <vazs.bruno@gmail.com>
2025-10-08 18:30:32 -0300

    feat: caso de teste com exceção

--- src/test/java/br/com/brunovaz/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/brunovaz/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -17,7 +17,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
+import br.com.brunovaz.gestao_vagas.exceptions.CompanyNotFoundException;
 import br.com.brunovaz.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.brunovaz.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.brunovaz.gestao_vagas.modules.company.repositories.CompanyRepository;
@@ -69,4 +72,21 @@ public class CreateJobControllerTest {
 
         System.out.println(result);
     }    
+
+    @Test
+    public void should_not_be_able_to_create_a_new_job_if_company_not_found() throws Exception{
+        var createJobDTO = CreateJobDTO.builder()
+            .benefits("BENEFITS TEST")
+            .descript("DESCRIPTION_TEST")
+            .level("LEVEL_TEST").build();
+
+            
+        mvc.perform( MockMvcRequestBuilders.post("/company/job/")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtils.objectToJSON(createJobDTO))
+            .header("Authorization", TestUtils.generateToken(UUID.randomUUID(),"JAVAGAS_@123#")))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+
+    }
 }
